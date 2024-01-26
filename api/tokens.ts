77f5ae4ccd26b2085892383e0f386d81391ec753
Marco Pacifico <marco.pacifico@gmail.com>
2024-01-26 16:52:08 -0300

    updating response header to meet figma plugin requirements

--- api/tokens.ts
+++ api/tokens.ts
@@ -2,6 +2,11 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'tokens' } = req.query
+
+  // Set the Access-Control-Allow-Origin header to allow all origins (required for Figma plugins)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+
   return res.json({
     message: `Hello ${name}!`,
   })
